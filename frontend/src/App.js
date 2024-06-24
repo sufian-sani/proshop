3855014bf34811a9837e183a0962811a4a95c19c
abu-sani <abu.sani@byslglobal.com>
2024-06-24 18:29:51 +0600

    6-24 6:29

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+// import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
