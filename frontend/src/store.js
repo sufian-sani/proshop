d0f26ce8fb6dc73b2bffbdbca5dd811eab3df002
abu-sani <abu.sani@byslglobal.com>
2024-07-04 15:21:01 +0600

    7-4 3:20

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,14 @@ import {configureStore} from '@reduxjs/toolkit'
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from './slices/cartSlice'
 import authSliceReducer from './slices/authSlice'
+import saveShippingAddress from './slices/cartSlice'
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReducer,
         auth: authSliceReducer,
+        shippingAddress: saveShippingAddress,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
